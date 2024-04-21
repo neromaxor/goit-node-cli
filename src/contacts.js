d22298b2d0516cf1cd45847da8a5e5f8436b6cc5
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-04-22 01:22:51 +0300

    update

---
+++ src/contacts.js
@@ -0,0 +1,58 @@
+const fs = require("node:fs").promises;
+
+const path = require("node:path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading contacts file:", error);
+    return [];
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.error("Error reading contacts file:", error);
+    return null;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    return true;
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error("Error removing contact:", error);
+    return false;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    const id = nanoid();
+    const newContact = { id, name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error("Error adding contact:", error);
+    return null;
+  }
+}
